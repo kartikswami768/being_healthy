55af2520ed8c2125ef1f7a8d66404f658dca957f
kartikswami768 <kartikswami768@gmail.com>
2026-09-09 03:55:59 +0000

    updated profile plugin type

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,15 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { componentRegistry } from "./quartz/components/registry"
+import Profile from "./quartz/components/Profile"
+
+componentRegistry.register("profile", Profile, "local", {
+  name: "profile",
+  displayName: "Profile",
+  description: "Displays the author's profile.",
+  version: "1.0.0",
+  defaultPosition: "left",
+  defaultPriority: 5,
+})
 
 const config = await loadQuartzConfig()
 export default config
