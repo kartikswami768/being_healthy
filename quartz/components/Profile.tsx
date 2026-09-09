ab2e95adff237a42f48349f0ca4cf92d2a6d527e
kartikswami768 <kartikswami768@gmail.com>
2026-09-09 03:43:33 +0000

    Corrected Profil.tsx

--- quartz/components/Profile.tsx
+++ quartz/components/Profile.tsx
@@ -1,34 +1,31 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
-import { classNames } from "./util"
 
-export default (() => {
-  const Profile: QuartzComponent = () => {
-    return (
-      <div class={classNames("profile")}>
-        <img src="/me.jpg" alt="Kartik Swami" />
-        <div class="profile-name">Kartik Swami</div>
-      </div>
-    )
-  }
+const Profile: QuartzComponent = () => {
+  return (
+    <div class="profile">
+      <img src="/me.jpg" alt="Kartik Swami" />
+      <div class="profile-name">Kartik Swami</div>
+    </div>
+  )
+}
 
-  Profile.css = `
-  .profile {
-    text-align: center;
-    margin-bottom: 1.5rem;
-  }
+Profile.css = `
+.profile {
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
 
-  .profile img {
-    width: 90px;
-    height: 90px;
-    border-radius: 50%;
-    object-fit: cover;
-  }
+.profile img {
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  object-fit: cover;
+}
 
-  .profile-name {
-    margin-top: 0.6rem;
-    font-weight: 600;
-  }
-  `
+.profile-name {
+  margin-top: 0.6rem;
+  font-weight: 600;
+}
+`
 
-  return Profile
-}) satisfies QuartzComponentConstructor
+export default (() => Profile) satisfies QuartzComponentConstructor
