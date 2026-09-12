ab69eccb03205f641cfcadc327e2ed24f3933c9f
kartikswami768 <kartikswami768@gmail.com>
2026-09-12 08:48:13 +0000

    Refactor FrontmatterDebug component to simplify return statement

--- quartz/components/FrontmatterDebug.tsx
+++ quartz/components/FrontmatterDebug.tsx
@@ -4,11 +4,7 @@ const FrontmatterDebug: QuartzComponent = ({ fileData }: QuartzComponentProps) =
   const frontmatter = fileData.frontmatter
   if (!frontmatter) return null
 
-  return (
-    <pre data-frontmatter-debug>
-      {JSON.stringify(frontmatter, null, 2)}
-    </pre>
-  )
+  return <pre data-frontmatter-debug>{JSON.stringify(frontmatter, null, 2)}</pre>
 }
 
 export default (() => FrontmatterDebug) satisfies QuartzComponentConstructor
