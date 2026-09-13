527992b5c3a0fea2517a00d189eaf811cecc51bd
kartikswami768 <kartikswami768@gmail.com>
2026-09-13 08:04:46 +0530

    Keep site identity out of desktop sidebar

--- quartz/components/Profile.tsx
+++ quartz/components/Profile.tsx
@@ -27,6 +27,12 @@ Profile.css = `
   line-height: 1.1;
 }
 
+@media all and (max-width: 800px) {
+  .mobile-header .profile-site-title {
+    display: block;
+  }
+}
+
 .profile-role {
   margin-top: 0.3rem;
   color: var(--site-muted);
@@ -42,12 +48,6 @@ Profile.css = `
   color: var(--site-heading);
   font-family: var(--headerFont);
   font-weight: 600;
-}
-
-@media all and (max-width: 800px) {
-  .mobile-header .profile-site-title {
-    display: block;
-  }
 }
 `
 
